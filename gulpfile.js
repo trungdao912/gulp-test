1457bd699a99f9be63a71030ffee0cfcd28d00cb
trungdao912 <trungdao912@gmail.com>
2018-10-14 20:28:50 +0700

    init

---
+++ gulpfile.js
@@ -0,0 +1,79 @@
+var gulp = require('gulp');
+var uglify = require('gulp-uglify');
+var livereload = require('gulp-livereload');
+var concat = require('gulp-concat');
+var minifyCss = require('gulp-minify-css');
+var autoprefixer = require('gulp-autoprefixer');
+var plumber = require('gulp-plumber');
+var sourcemaps = require('gulp-sourcemaps');
+var sass = require('gulp-sass');
+
+// File paths
+var DIST_PATH = 'public/dist';
+var SCRIPTS_PATH = 'public/scripts/**/*.js';
+var CSS_PATH = 'public/css/**/*.css';
+
+// // Styles
+// gulp.task('styles', function () {
+// 	console.log('starting styles task');
+// 	return gulp.src(['public/css/reset.css', CSS_PATH])
+// 		.pipe(plumber(function (err) {
+// 			console.log('Styles Task Error');
+// 			console.log(err);
+// 			this.emit('end');
+// 		}))
+// 		.pipe(sourcemaps.init())
+// 		.pipe(autoprefixer())
+// 		.pipe(concat('styles.css'))
+// 		.pipe(minifyCss())
+// 		.pipe(sourcemaps.write())
+// 		.pipe(gulp.dest(DIST_PATH))
+// 		.pipe(livereload());
+// });
+
+// Styles For SCSS
+gulp.task('styles', function () {
+	console.log('starting styles task');
+	return gulp.src('public/scss/styles.scss')
+		.pipe(plumber(function (err) {
+			console.log('Styles Task Error');
+			console.log(err);
+			this.emit('end');
+		}))
+		.pipe(sourcemaps.init())
+		.pipe(autoprefixer())
+		.pipe(sass({
+			outputStyle: 'compressed'
+		}))
+		.pipe(sourcemaps.write())
+		.pipe(gulp.dest(DIST_PATH))
+		.pipe(livereload());
+});
+
+// Scripts
+gulp.task('scripts', function () {
+	console.log('starting scripts task');
+
+	return gulp.src(SCRIPTS_PATH)
+		.pipe(uglify())
+		.pipe(gulp.dest(DIST_PATH))
+		.pipe(livereload());
+});
+
+// Images
+gulp.task('images', function () {
+	console.log('starting images task');
+});
+
+gulp.task('default', function () {
+	console.log('Starting default task');
+});
+
+gulp.task('watch', function () {
+	console.log('Starting watch task');
+	require('./server.js');
+	livereload.listen();
+	gulp.watch(SCRIPTS_PATH, ['scripts']);
+	// gulp.watch(CSS_PATH, ['styles']);
+	gulp.watch('public/scss/**/*.scss', ['styles']);
+});
